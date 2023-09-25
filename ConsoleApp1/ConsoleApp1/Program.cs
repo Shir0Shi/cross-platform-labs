fb4f00490b47532128a76ad03831911ea167ea0d
Shir0Shi <not.just.white@gmail.com>
2023-09-25 20:27:16 +0200

    lab 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] inputLines = File.ReadAllLines("INPUT.TXT");
+            string[] input = inputLines[0].Split();
+            int N = int.Parse(input[0]);
+            int K = int.Parse(input[1]);
+            int M = int.Parse(input[2]);
+
+            if (K <= 0 || K >= N || N >= 25 || M < 0 || M > N * (N - 1) / 2)
+            {
+                Console.WriteLine("Wrong inputs. Important: 0 < K < N < 25 и 0 ≤ M ≤ N(N-1)/2.");
+                return;
+            }
+
+
+            List<int>[] friends = new List<int>[N + 1];
+
+            for (int i = 0; i < M; i++)
+            {
+                string[] pair = inputLines[i + 1].Split();
+                int person1 = int.Parse(pair[0]);
+                int person2 = int.Parse(pair[1]);
+
+                if (friends[person1] == null)
+                    friends[person1] = new List<int>();
+                if (friends[person2] == null)
+                    friends[person2] = new List<int>();
+
+                friends[person1].Add(person2);
+                friends[person2].Add(person1);
+            }
+
+            List<int> team1 = new List<int>();
+            List<int> team2 = new List<int>();
+            List<int> currentTeam = new List<int>();
+            bool[] used = new bool[N + 1];
+
+            void Dfs(int v)
+            {
+                used[v] = true;
+                currentTeam.Add(v);
+
+                foreach (int u in friends[v])
+                {
+                    if (!used[u])
+                    {
+                        Dfs(u);
+                    }
+                }
+            }
+
+            for (int i = 1; i <= N; i++)
+            {
+                if (!used[i])
+                {
+                    currentTeam.Clear();
+                    Dfs(i);
+
+                    if (currentTeam.Count > team1.Count)
+                    {
+                        team2.AddRange(team1);
+                        team1.Clear();
+                        foreach (int person in currentTeam)
+                        {
+                            team1.Add(person);
+                        }
+                    }
+                    else
+                    {
+                        team2.AddRange(currentTeam);
+                    }
+                }
+            }
+
+            // Запись результата в файл OUTPUT.TXT
+            File.WriteAllText("OUTPUT.TXT", string.Join(" ", team1.Take(K)));
+        }
+    }
+}
