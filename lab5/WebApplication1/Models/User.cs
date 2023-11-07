e72e7e613a3de6a33a5fa652d3ceeca264145a2f
Shir0Shi <not.just.white@gmail.com>
2023-11-07 20:47:34 +0100

    beggining lab5

---
+++ lab5/WebApplication1/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
+        public string PasswordHash { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        
+    }
+}
